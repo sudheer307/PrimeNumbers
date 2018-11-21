5e861da2339113c45ce9f9281cdd3c71ce6697d9
sudheer <sudheer.ganatech308@gmail.com>
2018-11-21 14:16:08 +0530

    hi

---
+++ src/main/java/JavaExample.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class JavaExample {
+
+    public static void main(String[] args) {
+        String str;
+        System.out.println("Enter your username: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            /*       */str = scanner.nextLine();
+        }
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str)
+    {
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
